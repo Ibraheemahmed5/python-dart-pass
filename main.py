bcd3e8fa483081c92c64a2df1e52e5a4c7c729de
Ibraheem Ahmed <92911409+Ibraheemahmed5@users.noreply.github.com>
2022-04-09 18:45:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 x = []
 i = 0
 num = int(input("Enter X Value: "))
-while i < num:
-    num1 = int(input("Enter a Number: "))
-    i += 1
-    x += [num1]
+if num <= 10 and num > 0:
+    while i < num:
+        num1 = int(input("Enter A Number: "))
+        i += 1
+        x += [num1]
 for n in x:
-
     if (n % 2) == 0:
         print("{0} is Even".format(n))
     else:
         print("{0} is Odd".format(n))
+
+else:
+    print("OUT OF THE RANGE")
